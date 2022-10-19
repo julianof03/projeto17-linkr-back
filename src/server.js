2baf26bb9be0298f45da8f7a8342f034b0df251e
Sergio <sergiomendes23@gmail.com>
2022-10-19 10:34:43 -0300

    feat: merge

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+// import authRouters from './routers/authRouters.js';
+
+
+const server = express();
+server.use(cors());
+server.use(express.json());
+
+// server.use(authRouters);
+
+
+server.listen(process.env.PORT, () => {
+    console.log(`Magic happens on ${process.env.PORT}`);
+});

0536d834f278f5e08a34f7114687e4afd031e30f
belleomoraes <iolliveira.moraes@gmail.com>
2022-10-21 20:57:08 -0300

    feat get posts filtered by hashtags

--- src/controllers/hashtagController.js
+++ src/controllers/hashtagController.js
@@ -1,10 +1,10 @@
 import { connection } from "../database/database.js";
-
-//NÃO ESTÁ FINALIZADO
 async function getHashtagPosts(req, res) {
   const { hashtag } = req.params;
   try {
-    const filteredPosts = await connection.query("SELECT * FROM hashtags");
+    const filteredPosts = await connection.query(
+      'SELECT posts."userId", posts.text, posts.link, users.name, users."pictureUrl" FROM "hashPost" JOIN posts ON "hashPost"."postId" = posts.id JOIN hashtags ON "hashPost"."hashtagId" = hashtags.id JOIN users ON posts."userId" = users.id WHERE hashtags.name = $1', [hashtag]
+    );
     res.status(200).send(filteredPosts.rows);
   } catch (error) {
     res.status(500).send({ message: error.message });

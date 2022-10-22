8624d65fd2692d52fb521ed05cd5c5692c04f2f9
VictorCid <victorcidpereira@gmail.com>
2022-10-22 14:47:17 -0300

    oct22 repositories almost done

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,43 +1,48 @@
 import { connection } from "../database/database.js";
+import { usersRepository } from '../repositories/usersRepositories.js'
 
-export async function searchUsers(req,res){
-    const {startsWith} = req.params;
+export async function searchUsers(req, res) {
+    const { startsWith } = req.params;
     try {
-        const users = await connection.query(`
-        SELECT
-        "pictureUrl", name
-        FROM
-            users
-        WHERE
-            (lower(name) LIKE '${startsWith}%') 
-        ORDER BY
-        name
-        LIMIT
-        5`);
+        // const users = await connection.query(`
+        // SELECT
+        // "pictureUrl", name
+        // FROM
+        //     users
+        // WHERE
+        //     (lower(name) LIKE '${startsWith}%') 
+        // ORDER BY
+        // name
+        // LIMIT
+        // 5`);
+
+        const users = await usersRepository.searchUser(startsWith)
 
         return res.status(200).send(users.rows)
-        
+
     } catch (error) {
         res.status(501).send({ message: error.message });
     }
 };
 
-export async function userInfo(req,res){
+export async function userInfo(req, res) {
     const userId = res.locals.userId;
 
     try {
-        const user = await connection.query(`
-        SELECT
-            "pictureUrl"
-        FROM
-            users
-        WHERE
-            id =$1
-        LIMIT
-        1`,[userId]);
+        // const user = await connection.query(`
+        // SELECT
+        //     "pictureUrl"
+        // FROM
+        //     users
+        // WHERE
+        //     id =$1
+        // LIMIT
+        // 1`, [userId]);
+
+        const user = await usersRepository.getPicture(userId)
 
         return res.status(200).send(user.rows);
-        
+
     } catch (error) {
         res.status(501).send({ message: error.message });
     }
---
+++ src/repositories/hashtagRepositories.js
@@ -0,0 +1,24 @@
+import { connection } from "../database/database.js";
+
+async function getHashtagPosts(hashtag) {
+    const query = `
+    SELECT 
+        posts."userId",
+        posts.text,
+        posts.link,
+        users.name,
+        users."pictureUrl"
+    FROM "hashPost" 
+    JOIN posts ON "hashPost"."postId" = posts.id 
+    JOIN hashtags ON "hashPost"."hashtagId" = hashtags.id 
+    JOIN users ON posts."userId" = users.id 
+    WHERE hashtags.name = $1`
+    return connection.query(query, [hashtag])
+
+}
+
+hashtagRepository = {
+    getHashtagPosts
+}
+
+export { hashtagRepository }

06562317a6949ef07cab104fab218d9e9f830f32
julianof03 <jlprod08@gmail.com>
2023-06-12 02:34:27 -0300

    fix/db

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -5,6 +5,7 @@ import {connection} from '../database/database.js'
 
 export function validateSignUp(req, res, next) {
     const customer = req.body;
+    console.log("im here")
 
     const { error } = signUpSchema.validate(customer, { abortEarly: false });
 
